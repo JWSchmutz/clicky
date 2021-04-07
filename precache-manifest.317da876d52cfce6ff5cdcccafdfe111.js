37af33360b8b8bb60b3cbecd8bb7311d93d0e9b3
Jeremiah Schmutz <Jeremiah.Schmutz@gmail.com>
2021-04-07 11:50:25 -0400

    Updates

---
+++ precache-manifest.317da876d52cfce6ff5cdcccafdfe111.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f49af2b13b555eb8ef39e667a5f1c0",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "5e918f70f98f445839fd",
+    "url": "/clicky/static/css/main.f6c3c3a2.chunk.css"
+  },
+  {
+    "revision": "ddb3942de7276520549d",
+    "url": "/clicky/static/js/2.c80fa807.chunk.js"
+  },
+  {
+    "revision": "5e918f70f98f445839fd",
+    "url": "/clicky/static/js/main.8a0d3368.chunk.js"
+  },
+  {
+    "revision": "48fb82b10597630f3b6b",
+    "url": "/clicky/static/js/runtime~main.0d6b5fa3.js"
+  },
+  {
+    "revision": "f7a32113a2691c8931618392a4940d36",
+    "url": "/clicky/static/media/davis.f7a32113.jpg"
+  },
+  {
+    "revision": "6a1694c04adbcac5c5a2487bb9ca43d1",
+    "url": "/clicky/static/media/durant.6a1694c0.jpg"
+  },
+  {
+    "revision": "e7c19b57a1c9b710facbddac1bcaad48",
+    "url": "/clicky/static/media/giannis.e7c19b57.jpg"
+  },
+  {
+    "revision": "dcb4a25aabde373dd224fc5db164fc6d",
+    "url": "/clicky/static/media/harden.dcb4a25a.jpg"
+  },
+  {
+    "revision": "fb3780fe70acd353e0600f32fa54f9f3",
+    "url": "/clicky/static/media/kawhi.fb3780fe.jpg"
+  },
+  {
+    "revision": "da97028cbf2d8218a2a8dff0f2ffefae",
+    "url": "/clicky/static/media/lebron.da97028c.jpg"
+  },
+  {
+    "revision": "4d3a783672702c0ad80138399bbf3aa8",
+    "url": "/clicky/static/media/luka.4d3a7836.jpg"
+  },
+  {
+    "revision": "a0a4d04a7b037b25196116a2e54c861a",
+    "url": "/clicky/static/media/stephen.a0a4d04a.jpg"
+  }
+]);
